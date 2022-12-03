1f4297f8576d0fcbe20531d25d88b9dea97acf17
idanfadlon <66566962+idanfadlon@users.noreply.github.com>
2022-12-03 15:54:35 +0200

    Correcting a typo in the slogan Splash screen update Logo replacement in sign in and sign up and fix the validation by user type

--- app/src/main/java/com/example/law_tech_app/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/law_tech_app/activities/SignUpActivity.kt
@@ -11,8 +11,10 @@ import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
+import android.widget.Toast
 import com.example.law_tech_app.R
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
@@ -44,9 +46,14 @@ class SignUpActivity : com.example.law_tech_app.activities.BaseActivity() {
         radioGroup.setOnCheckedChangeListener{_,checkedId->
             onRadioClick(checkedId)
         }
+        tieFullName=findViewById(R.id.tie_fullName)
+        tieEmail=findViewById(R.id.tie_email)
+        tiePassword=findViewById(R.id.tie_password)
+        tieConfirmPassword=findViewById(R.id.tie_confirmPassword)
+        tiePhoneNumber=findViewById(R.id.tie_phoneNumber)
         tieLicenseNumber=findViewById(R.id.tie_licenseNumber)
         tieSummary=findViewById(R.id.tie_summary)
-         btnSignUp=findViewById(R.id.btn_signup)
+        btnSignUp=findViewById(R.id.btn_signup)
         btnSignUp.setOnClickListener{
             signUpUser()
         }
@@ -80,37 +87,25 @@ class SignUpActivity : com.example.law_tech_app.activities.BaseActivity() {
     }
 
     private fun signUpUser(){
-        if (validateSignUpDetails()){
-            val email: String = tieEmail.text.toString().trim { it <= ' ' }
-            val password: String = tiePassword.text.toString().trim { it <= ' ' }
-
-            // Create an instance and create a register a user with email and password.
-            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(
-                    OnCompleteListener<AuthResult> { task ->
-
-                        // If the registration is successfully done
-                        if (task.isSuccessful) {
-
-                            // Firebase registered user
-                            val firebaseUser: FirebaseUser = task.result!!.user!!
-
-                            showErrorSnackBar(
-                                "signup has been successfully completed",
-                                false
-                            )
-                        } else {
-                            // If the registering is not successful then show error message.
-                            showErrorSnackBar(task.exception!!.message.toString(), true)
-                        }
-                    })
-        }
-
+        val radiochecked=findViewById<RadioButton>(R.id.rb_lawyer)
+        if(radiochecked.isChecked)
+            if (validateLawyerSignUpDetails()) {
+                createUserFirebase()
+            }
+        else {
+                if (validateClientSignUpDetails()) {
+                    createUserFirebase()
+                }
+            }
     }
-    private fun validateSignUpDetails():Boolean {
+    private fun validateClientSignUpDetails():Boolean {
 
             return when {
                 TextUtils.isEmpty(tieFullName.text.toString().trim { it <= ' ' }) -> {
+                    showErrorSnackBar("Please enter full name", true)
+                    false
+                }
+                TextUtils.isEmpty(tieEmail.text.toString().trim { it <= ' ' }) -> {
                     showErrorSnackBar("Please enter full name", true)
                     false
                 }
@@ -134,7 +129,6 @@ class SignUpActivity : com.example.law_tech_app.activities.BaseActivity() {
                     showErrorSnackBar("passwords are not matched",true)
                     false
                 }
-                //TODO: think how recognaized type of the user
                 else -> {
                    true
 
@@ -142,4 +136,72 @@ class SignUpActivity : com.example.law_tech_app.activities.BaseActivity() {
             }
 
         }
+    private fun validateLawyerSignUpDetails():Boolean{
+        return when {
+            TextUtils.isEmpty(tieFullName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar("Please enter full name", true)
+                false
+            }
+            TextUtils.isEmpty(tieEmail.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar("Please enter full name", true)
+                false
+            }
+            TextUtils.isEmpty(tiePassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar("Please enter password", true)
+                false
+            }
+            TextUtils.isEmpty(tieConfirmPassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar("Please re-enter password", true)
+                false
+            }
+            TextUtils.isEmpty(tiePhoneNumber.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar("Please enter phone number", true)
+                false
+            }
+
+            TextUtils.isEmpty(tieLicenseNumber.text.toString().trim { it<=' ' })->{
+                showErrorSnackBar("Please enter license number",true)
+                false
+            }
+
+            TextUtils.isEmpty(tieSummary.text.toString().trim(){ it<=' '})->{
+                showErrorSnackBar("Please tell a little about yourself",true)
+                false
+            }
+            tiePassword.text.toString().length<6 ->{
+                showErrorSnackBar("password must include at last 6 characters",true)
+                false
+            }
+            tiePassword.text.toString()!=tieConfirmPassword.text.toString() ->{
+                showErrorSnackBar("passwords are not matched",true)
+                false
+            }
+            else -> {true}
+        }
+
     }
+    private fun createUserFirebase(){
+        val email: String = tieEmail.text.toString().trim { it <= ' ' }
+        val password: String = tiePassword.text.toString().trim { it <= ' ' }
+
+        // Create an instance and create a register a user with email and password.
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(
+                OnCompleteListener<AuthResult> { task ->
+
+                    // If the registration is successfully done
+                    if (task.isSuccessful) {
+
+                        // Firebase registered user
+                        val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                        Toast.makeText(this,"sign up succss",Toast.LENGTH_SHORT).show()
+                    } else {
+                        // If the registering is not successful then show error message.
+                        showErrorSnackBar(task.exception!!.message.toString(), true)
+                    }
+                })
+
+    }
+
+}
