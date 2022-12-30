76bb85bebcbdd8e005fada60a6cd5ec9ebd02112
matan15595m <matan15595m@gmail.com>
2022-12-30 13:22:10 +0200

    Login and sign up fixed finally
    Login and sign up fixed for both types and finally working

--- app/src/main/java/com/example/law_tech_app/fragments/BaseFragment.kt
+++ app/src/main/java/com/example/law_tech_app/fragments/BaseFragment.kt
@@ -10,18 +10,18 @@ import com.example.law_tech_app.R
 import kotlinx.android.synthetic.main.dialog_progress.*
 
 
-open class BaseFragment : Fragment() {
-    private var mProgressDialog:Dialog = Dialog(requireActivity())
-
-    fun showProgressDialog(text:String){
-        mProgressDialog.setContentView(R.layout.dialog_progress)
-        mProgressDialog.tv_progress_text.text = text
-        mProgressDialog.setCancelable(false)
-        mProgressDialog.setCanceledOnTouchOutside(false)
-        mProgressDialog.show()
-    }
-    fun hideProgressDialog(){
-        mProgressDialog.dismiss()
-    }
-
-}
+//open class BaseFragment : Fragment() {
+//    private var mProgressDialog:Dialog = Dialog(requireActivity())
+//
+//    fun showProgressDialog(text:String){
+//        mProgressDialog.setContentView(R.layout.dialog_progress)
+//        mProgressDialog.tv_progress_text.text = text
+//        mProgressDialog.setCancelable(false)
+//        mProgressDialog.setCanceledOnTouchOutside(false)
+//        mProgressDialog.show()
+//    }
+//    fun hideProgressDialog(){
+//        mProgressDialog.dismiss()
+//    }
+//
+//}
